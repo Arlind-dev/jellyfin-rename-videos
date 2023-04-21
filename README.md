a13027ddf3f80a2c0c089a558b21bf5b89c74e40
Arlind-dev <sulejmaniarlind5@gmail.com>
2023-04-21 10:41:26 +0200

    Push final

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# jellyfin-rename-videos
-renames jellyfin videos so metadata can be found more easily
+# Jellyfin Rename Videos
+
+This Python script renames video files in subdirectories named "Season XX" to the format "SEXXEYY.ext", where "XX" is the season number and "YY" is the episode number, sorted naturally.
+
+## Requirements
+
+- Python 3
+- natsort package (pip install natsort)
+
+## Usage
+
+1. Clone the repository or download the script `jellyfin_rename.py`.
+2. Open a command prompt or terminal in the directory containing the script.
+3. Run the script by typing python `jellyfin_rename.py`.
+4. Enter the folder path to work in when prompted, or leave it blank for the current directory.
+5. Enter the file extension to filter by when prompted, or leave it blank for `.mkv`.
+6. The script will display the old and new filenames for each video file to be renamed, sorted by season and episode number. Confirm whether you want to proceed with renaming the files for each season.
+7. The script will rename the files according to the specified naming convention.
+
+## Example
+
+Suppose you have the following folder structure:
+
+```bash
+jellyfin_rename_videos/
+├── Season 01/
+│   ├── video1.mkv
+│   ├── video2.mkv
+│   └── video3.mkv
+└── Season 02/
+    ├── video4.mkv
+    ├── video5.mkv
+    └── video6.mkv
+```
+
+You run the script by typing `python jellyfin_rename_videos.py`, and enter `mkv` (or leave it blank) for the file extension. The script will display the following:
+
+```bash
+Enter a folder path (default is current directory):
+Enter a file extension (default is mkv):
+./Season 01/video1.mkv --> ./Season 01/SE01EP01.mkv
+./Season 01/video2.mkv --> ./Season 01/SE01EP02.mkv
+./Season 01/video3.mkv --> ./Season 01/SE01EP03.mkv
+Do you want to proceed with renaming the files in Season 01? (y/n):
+Done renaming files in Season 01.
+./Season 02/video4.mkv --> ./Season 02/SE02EP01.mkv
+./Season 02/video5.mkv --> ./Season 02/SE02EP02.mkv
+./Season 02/video6.mkv --> ./Season 02/SE02EP03.mkv
+Do you want to proceed with renaming the files in Season 02? (y/n):
+Done renaming files in Season 02.
+All files renamed.
+```
+After confirming that you want to proceed with renaming the files for each season, the script will rename the files as follows:
+
+```bash
+jellyfin_rename_videos/
+├── Season 01/
+│   ├── SE01EP01.mkv
+│   ├── SE01EP02.mkv
+│   └── SE01EP03.mkv
+└── Season 02/
+    ├── SE02EP01.mkv
+    ├── SE02EP02.mkv
+    └── SE02EP03.mkv
+```
---
+++ jellyfin_rename.py
@@ -0,0 +1,67 @@
+import os
+from natsort import natsort_keygen
+
+# Ask the user for a folder path to work in
+folder_path = input("Enter a folder path (default is current directory): ")
+
+# Check if the user entered a value or not
+if not folder_path:
+    folder_path = "."  # Set default value to current directory
+
+# Resolve the folder path to an absolute path using os.path.abspath()
+folder_path = os.path.abspath(folder_path)
+
+# Check if the folder exists
+if not os.path.exists(folder_path):
+    print(f"The folder {folder_path} does not exist.")
+    exit()
+
+# Ask the user for a file extension to filter by
+file_ext = input("Enter a file extension (default is mkv): ")
+
+# Set default value to mkv if the user does not enter a value
+if not file_ext:
+    file_ext = "mkv"
+
+# Create a list of all subdirectories containing "Season" in their name
+season_dirs = [d for d in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, d)) and "Season" in d]
+
+# Iterate through each season directory
+for season_dir in season_dirs:
+    season_number_str = season_dir.split()[-1]
+    try:
+        season_number = int(season_number_str)
+    except ValueError:
+        print(f"Invalid season number: {season_number_str}")
+        continue
+
+    # List the files in the season directory with the specified extension, sorted naturally
+    season_dir_path = os.path.join(folder_path, season_dir)
+    nkey = natsort_keygen()
+    files = sorted([f for f in os.listdir(season_dir_path) if f.lower().endswith("." + file_ext.lower())], key=nkey)
+
+    # Show the user the new filenames
+    for i, filename in enumerate(files, start=1):
+        old_path = os.path.join(season_dir_path, filename)
+        new_filename = f"SE{season_number:02d}EP{i:02d}.{file_ext}"
+        new_path = os.path.join(season_dir_path, new_filename)
+        rel_old_path = os.path.relpath(old_path, start=folder_path)
+        rel_new_path = os.path.relpath(new_path, start=folder_path)
+        print(f".{os.path.sep}{rel_old_path} --> .{os.path.sep}{rel_new_path}")
+
+    # Ask the user if they want to proceed with renaming the files
+    confirmationRename = input(f"Do you want to proceed with renaming the files in {season_dir}? (y/n): ").strip().lower() or "y"
+    if confirmationRename != "y":
+        print(f"File renaming cancelled for {season_dir}.")
+        continue
+
+    # Rename the files according to the specified naming convention
+    for i, filename in enumerate(files, start=1):
+        old_path = os.path.join(season_dir_path, filename)
+        new_filename = f"SE{season_number:02d}EP{i:02d}.{file_ext}"
+        new_path = os.path.join(season_dir_path, new_filename)
+        os.rename(old_path, new_path)
+
+    print(f"Done renaming files in {season_dir}.")
+
+print("done")
