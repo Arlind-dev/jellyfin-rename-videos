fa9fec23f9065d294a49e6900ac5e6fdee41472f
Arlind-dev <sulejmaniarlind5@gmail.com>
2023-07-05 11:05:21 +0200

    rename variables, add table output

--- jellyfin_rename.py
+++ jellyfin_rename.py
@@ -1,9 +1,10 @@
-# Version: 1.0.4
+# Version: 1.0.5
 # Contributors: Arlind-dev
 # Python 3.11.0
 
 import os
 from natsort import natsort_keygen
+from tabulate import tabulate
 
 # Ask the user for a directory path to work in
 directory_path = input("Enter a directory path (default is current directory): ")
@@ -41,6 +42,9 @@ file_ext = input("Enter a file extension (default is mkv): ")
 if not file_ext:
     file_ext = "mkv"
 
+# Store the renaming information in a list of lists
+renaming_info = []
+
 # Iterate through each season directory
 for season_directory in season_directories:
     season_number_str = season_directory.split()[-1]
@@ -51,44 +55,54 @@ for season_directory in season_directories:
         print("'Season' directory can only contain 2 Digits (00-99)")
         continue
 
-    print("")
+    season_info = []
 
     # List the files in the season directory with the specified extension, sorted naturally
     season_directory_path = os.path.join(directory_path, season_directory)
-    nkey = natsort_keygen()
+    natural_sort_key = natsort_keygen()
     files = sorted(
         [
             f
             for f in os.listdir(season_directory_path)
             if f.lower().endswith("." + file_ext.lower())
         ],
-        key=nkey,
+        key=natural_sort_key,
     )
 
-    # Check if the number of files is a three-digit number
-    if len(files) > 99:
-        new_file_ext = f"S{season_number:02d}E{{:03d}}.{file_ext}"
-    else:
-        new_file_ext = f"S{season_number:02d}E{{:02d}}.{file_ext}"
+    # Determine the number of digits needed based on the number of files
+    num_digits = max(2, len(str(len(files))))
+
+    # Construct the new file extension format string
+    new_file_ext = f"S{season_number:02d}E{{:0{num_digits}d}}.{file_ext}"
 
     # Show the user the new filenames
-    renamed = False
     for i, filename in enumerate(files, start=1):
         old_path = os.path.join(season_directory_path, filename)
         new_filename = new_file_ext.format(i)
         new_path = os.path.join(season_directory_path, new_filename)
         rel_old_path = os.path.relpath(old_path, start=directory_path)
         rel_new_path = os.path.relpath(new_path, start=directory_path)
-        print(f".{os.path.sep}{rel_old_path}\t -->\t .{os.path.sep}{rel_new_path}")
-        if filename != new_filename:
-            renamed = True
+        season_info.append(
+            [f".{os.path.sep}{rel_old_path}", f".{os.path.sep}{rel_new_path}"]
+        )
 
+    renaming_info.append(season_info)
+
+    renamed = any(
+        filename != new_file_ext.format(i) for i, filename in enumerate(files, start=1)
+    )
     if not renamed:
-        print(f"Episodes in {season_directory} dont need to be renamed.")
+        print(f"Episodes in {season_directory} don't need to be renamed.")
         continue
 
+    # Print the renaming information as a table
+    print(f"\nRenaming Information for {season_directory}:")
+    print(
+        tabulate(season_info, headers=["Old Path", "New Path"], tablefmt="fancy_grid")
+    )
+
     # Ask the user if they want to proceed with renaming the files
-    confirmationRename = (
+    confirmation_rename = (
         input(
             f"Do you want to proceed with renaming the files in {season_directory}? (y/n): "
         )
@@ -96,16 +110,15 @@ for season_directory in season_directories:
         .lower()
         or "y"
     )
-    if confirmationRename != "y":
+    if confirmation_rename != "y":
         print(f"File renaming cancelled for {season_directory}.")
         continue
-        print("")
-    # Rename the files according to the specified naming convention
-    for i, filename in enumerate(files, start=1):
-        old_path = os.path.join(season_directory_path, filename)
-        new_filename = new_file_ext.format(i)
-        new_path = os.path.join(season_directory_path, new_filename)
-        os.rename(old_path, new_path)
 
-print("")
-print("Done.")
+    # Rename the files according to the specified naming convention
+    for j, (old_path, new_path) in enumerate(season_info, start=1):
+        os.rename(
+            os.path.join(directory_path, old_path),
+            os.path.join(directory_path, new_path),
+        )
+
+print("\nDone.")
